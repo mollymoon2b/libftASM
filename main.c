8953902d20af15253827cf2a514d6e26c0c3acd4
Alice DE-BONNECHOSE <ade-bonn@e2r1p9.42.fr>
2015-02-11 14:57:04 +0100

    main

---
+++ main.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-bonn <ade-bonn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/11 12:07:10 by ade-bonn          #+#    #+#             */
+/*   Updated: 2015/02/11 14:42:19 by ade-bonn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+// ft_isnum.s
+
+/*
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return 0;
+	int i = 12;
+	i = ft_isnum(av[1][0]);
+	printf("i = [%d]\n", i);
+	return (0);
+}
+*/
+
+ //ft_isalpha.s
+ 
+ /*
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        return 0;
+    int i = 12;
+    i = ft_isalpha(av[1][0]);
+    printf("i = [%d]\n", i);
+    return (0);
+} */
+
+ //ft_isalnum.s
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        return 0;
+    int i = 12;
+    i = ft_isalnum(av[1][0]);
+    printf("i = [%d]\n", i);
+    return (0);
+}
